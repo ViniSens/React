c752d3199bdbdc60dfd17d29a18e5e920bc24e32
vinicius <>
2022-02-23 17:00:40 -0300

    Projeto Task Manager

---
+++ taskmanager/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Button.css"
+
+const Button = ({children, onClick}) => {
+    return ( 
+        <button onClick={onClick} className="add-button">
+            {children}
+        </button>
+     );
+}
+ 
+export default Button;
